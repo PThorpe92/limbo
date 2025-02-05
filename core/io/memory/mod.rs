3a136a4949b3ed2d5a61e0225df30d6baff56c01
pedrocarlo <pedro.muniz.carlo@gmail.com>
2025-02-05 03:02:53 -0300

    checkpoint experimentation
    wip: checkpoint testing for performance
    
    mass insert test
    
    fix: resizing too much incorrectly. Changed resize condition
    
    comment on mmap persistance to disk
    
    use remap for linux
    
    fix add cfg for RemapOptions
    
    cleanup comments
    
    add mass-insert-delete test
    
    bigger memory tests
    
    correct memory test amount of inserts
    
    change back to 100 inserts so that CI can run properly
    
    changed from tcl to python to test for memory
    
    adjusted amount of tests generated to speed up testing
    
    create ci flag to run in tests so that limbo is built with less pages. This makes the program resize more pages so that this behavior can be tested
    
    changed ci flag to be contained in OnceCell
    
    cleanup
    
    remove unnecessary whitespace
    
    remove ci flag in workflow and modify memory test to account for that
    
    rename env var and add some documentation
    
    remove log from file
    
    removed memmap2 dependency in favor of rustix
    
    Fix comment: new -> old
    
    Comment about tx visibility when deleting a row
    
    refactor: is_version_visible() -> RowVersion::is_visible_to()
    
    thank you clippy, actually a nice suggestion
    
    cleanup and correct remap call
    
    adding rustix to be a dependency for everything
    
    reverting to memmap2 for windows and wasi builds as rustix::mm does not support these plataforms
    
    adjust builds for wasm
    
    trying to add errno crate to compile wasm
    
    change to use crate result error
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    adjusting len of map after remap

--- core/io/memory/mod.rs
+++ core/io/memory/mod.rs
@@ -1,11 +1,26 @@
+#[cfg(target_family = "unix")]
+mod mmap_rustix;
+
+#[cfg(target_family = "unix")]
+use mmap_rustix::MmapAnon;
+
+#[cfg(not(target_family = "unix"))]
+mod mmap;
+
+#[cfg(not(target_family = "unix"))]
+use mmap::MmapAnon;
+
 use super::{Buffer, Completion, File, OpenFlags, IO};
 use crate::Result;
 
 use std::{
-    cell::{Cell, RefCell, UnsafeCell},
+    cell::{Cell, OnceCell, RefCell, UnsafeCell},
     collections::BTreeMap,
+    rc::Rc,
+    rc::Rc,
     sync::Arc,
 };
+
 use tracing::debug;
 
 pub struct MemoryIO {}
@@ -13,13 +28,106 @@ unsafe impl Send for MemoryIO {}
 
 // TODO: page size flag
 const PAGE_SIZE: usize = 4096;
-type MemPage = Box<[u8; PAGE_SIZE]>;
+type MemPage = [u8];
+const INITIAL_PAGES: OnceCell<usize> = OnceCell::new();
+const DEFAULT_INITIAL_PAGES: usize = 16;
 
-impl MemoryIO {
+impl MemoryFile {
     #[allow(clippy::arc_with_non_send_sync)]
     pub fn new() -> Self {
         debug!("Using IO backend 'memory'");
-        Self {}
+        Self {
+            #[cfg(target_family = "unix")]
+            pages: MemoryPages,
+        }
+    }
+
+    #[allow(clippy::mut_from_ref)]
+    fn get_or_allocate_page(&self, page_no: usize) -> &mut MemPage {
+        unsafe {
+            let pages = &mut *self.pages.get();
+            pages
+                .entry(page_no)
+                .or_insert_with(|| Box::new([0; PAGE_SIZE]))
+        }
+    }
+
+    fn get_page(&self, page_no: usize) -> Option<&MemPage> {
+        unsafe { (*self.pages.get()).get(&page_no) }
+        // INITIAL PAGES changes in CI to test for resizing of memory pages
+        let initial_pages = INITIAL_PAGES
+            .get_or_init(|| {
+                std::env::var("INITIAL_MEM_PAGES").map_or(DEFAULT_INITIAL_PAGES, |str_num| {
+                    let initial_pages = str_num.parse::<usize>().unwrap_or(DEFAULT_INITIAL_PAGES);
+                    if initial_pages == 0 {
+                        panic!("INITIAL_MEM_PAGES flag cannot be zero")
+                    }
+                    initial_pages
+                })
+            })
+            .clone();
+
+        Ok(Arc::new(Self {
+            size: 0.into(),
+            // Initial default size of 4kb * 16 pages = 64kb
+            pages: MmapAnon::new(PAGE_SIZE * initial_pages)?.into(),
+        }))
+    }
+
+    #[allow(clippy::mut_from_ref)]
+    fn get_or_allocate_page(&self, page_no: usize) -> Result<&mut MemPage> {
+        let start = page_no * PAGE_SIZE;
+        let end = start + PAGE_SIZE;
+        let len_pages = unsafe {
+            let page = &mut *self.pages.get();
+            page.len()
+        };
+
+        if end > len_pages {
+            let cap_pages = len_pages / PAGE_SIZE;
+            self.resize(cap_pages * 2)?;
+        }
+
+        let page = unsafe {
+            let page = &mut *self.pages.get();
+            &mut page[start..end]
+        };
+        Ok(page)
+    }
+
+    fn get_page(&self, page_no: usize) -> Option<&MemPage> {
+        let start = page_no * PAGE_SIZE;
+        let end = start + PAGE_SIZE;
+        let pages = unsafe { &mut *self.pages.get() };
+
+        // Make sure that we are accessing into a valid memory page
+        assert!(end <= pages.len());
+
+        let page = &pages[start..end];
+        if page.is_empty() {
+            None
+        } else {
+            Some(page)
+        }
+    }
+
+    fn resize(&self, capacity: usize) -> Result<()> {
+        let pages = unsafe { &mut *self.pages.get() };
+        let new_cap = PAGE_SIZE * capacity;
+        #[cfg(target_os = "linux")]
+        {
+            pages.remap(new_cap)?;
+        }
+
+        #[cfg(not(target_os = "linux"))]
+        {
+            let mut new_pages = MmapAnon::new(new_cap)?;
+            new_pages[..pages.len()].clone_from_slice(pages);
+
+            *pages = new_pages;
+        }
+
+        Ok(())
     }
 }
 
@@ -57,6 +165,7 @@ pub struct MemoryFile {
     pages: UnsafeCell<BTreeMap<usize, MemPage>>,
     size: Cell<usize>,
 }
+
 unsafe impl Send for MemoryFile {}
 unsafe impl Sync for MemoryFile {}
 
@@ -99,7 +208,6 @@ impl File for MemoryFile {
                 } else {
                     read_buf.as_mut_slice()[buf_offset..buf_offset + bytes_to_read].fill(0);
                 }
-
                 offset += bytes_to_read;
                 buf_offset += bytes_to_read;
                 remaining -= bytes_to_read;
@@ -159,21 +267,9 @@ impl File for MemoryFile {
 impl Drop for MemoryFile {
     fn drop(&mut self) {
         // no-op
-    }
-}
-
-impl MemoryFile {
-    #[allow(clippy::mut_from_ref)]
-    fn get_or_allocate_page(&self, page_no: usize) -> &mut MemPage {
-        unsafe {
-            let pages = &mut *self.pages.get();
-            pages
-                .entry(page_no)
-                .or_insert_with(|| Box::new([0; PAGE_SIZE]))
-        }
-    }
-
-    fn get_page(&self, page_no: usize) -> Option<&MemPage> {
-        unsafe { (*self.pages.get()).get(&page_no) }
+        // TODO ideally we could have some flags
+        // in Memory File, that when this is dropped
+        // if the persist flag is true we could flush the mmap to disk
+        // We would also have to store the file name here in this case
     }
 }
