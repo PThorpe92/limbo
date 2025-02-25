0a48e753aa2a4fb5e9a6a1ba8d15b4090a6f1184
PThorpe92 <preston@unlockedlabs.org>
2025-02-25 09:53:53 -0500

    Organize and clean up extension library, separate modules

---
+++ extensions/core/src/functions.rs
@@ -0,0 +1,39 @@
+pub type ScalarFunction = unsafe extern "C" fn(argc: i32, *const Value) -> Value;
+
+pub type RegisterScalarFn =
+    unsafe extern "C" fn(ctx: *mut c_void, name: *const c_char, func: ScalarFunction) -> ResultCode;
+
+pub type RegisterAggFn = unsafe extern "C" fn(
+    ctx: *mut c_void,
+    name: *const c_char,
+    args: i32,
+    init: InitAggFunction,
+    step: StepFunction,
+    finalize: FinalizeFunction,
+) -> ResultCode;
+
+pub type RegisterModuleFn = unsafe extern "C" fn(
+    ctx: *mut c_void,
+    name: *const c_char,
+    module: VTabModuleImpl,
+    kind: VTabKind,
+) -> ResultCode;
+
+pub type InitAggFunction = unsafe extern "C" fn() -> *mut AggCtx;
+pub type StepFunction = unsafe extern "C" fn(ctx: *mut AggCtx, argc: i32, argv: *const Value);
+pub type FinalizeFunction = unsafe extern "C" fn(ctx: *mut AggCtx) -> Value;
+
+#[repr(C)]
+pub struct AggCtx {
+    pub state: *mut c_void,
+}
+
+pub trait AggFunc {
+    type State: Default;
+    type Error: Display;
+    const NAME: &'static str;
+    const ARGS: i32;
+
+    fn step(state: &mut Self::State, args: &[Value]);
+    fn finalize(state: Self::State) -> std::result::Result<Value, Self::Error>;
+}
